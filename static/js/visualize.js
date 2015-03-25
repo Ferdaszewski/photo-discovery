34463eda60e68927fd62c1cef733205c0d0b3eff
Joshua Ferdaszewski <ferdaszewski@gmail.com>
2015-03-25 16:18:34 -0700

    Visulization front-end working!

--- static/js/visualize.js
+++ static/js/visualize.js
@@ -1,14 +1,24 @@
+// Instantiate a new imagesLoaded object
 var imgLoad = new imagesLoaded( '#album-wrapper' );
+
+// Set variables to track number of photos loaded/total and display them
 var imgs_loaded = 0;
+var img_total = $( '#album-wrapper' ).find( 'img' ).length;
+$( '#pl-total' ).text( img_total );
+$( '#pl-counter' ).text( imgs_loaded );
 
+
+// Set event listeners on imagesLoaded events
 imgLoad.on( 'always', function( instance ) {
-    $('#pre-load').fadeOut('slow', function() {
-        $('#album-wrapper').fadeIn('slow');
-    });
-    
-    // show images now that they are loaded
-    console.log("ALWAYS - all images loaded.")
 
+    // Images are finished loading, remove loading info and show images
+    $( '#pre-load' ).fadeOut( 'slow', function() {
+        $( '.thumbscrubber' ).fadeIn( 'slow' );
+
+        // Remove pre-load DOM node as we no longer need it
+        $( this ).remove();
+        albumScrubber();
+    });
 });
 
 imgLoad.on( 'done', function( instance ) {
@@ -20,13 +30,42 @@ imgLoad.on( 'fail', function( instance ) {
 });
 
 imgLoad.on( 'progress', function( instance, image ) {
-    if (image.isLoaded) {
+
+    // Display the new number of images successfully loaded
+    if ( image.isLoaded ) {
         imgs_loaded += 1;
     }
-    $('#pl-counter').text(imgs_loaded)
+    $( '#pl-counter' ).text(imgs_loaded)
 });
 
-// get total number of images in this album and display
-var img_total = $('#album-wrapper').find('img').length;
-$('#pl-total').text(img_total);
-$('#pl-counter').text(imgs_loaded);
+
+// Image Scrubber
+// Based on an idea from Sage Arslan. http://zurb.com/forrst/posts/iPhoto_like_Thumbnail_Scrubber_with_jQuery-Gc2
+function albumScrubber() {
+    var as_previous;
+    var as_current = 1;
+    changeImage();
+
+    var width = $( '#album-wrapper' ).innerWidth();
+    var numslides = img_total;
+
+    function changeImage() {
+        $( '#album-inner > .as-current' ).removeClass( 'as-current' );
+        $( '#album-inner > :nth-child(' + as_current + ')' ).addClass( 'as-current' );
+
+        // Change background color to match displayed image
+        var hexrgb = $( '#album-inner > .as-current' ).data( 'hexrgb' )
+        $( 'body' ).css( 'background-color' , hexrgb );
+    };
+
+    $( document ).on( 'mousemove', 'body', function( e ) {
+        x = e.pageX - $(this).offset().left;
+        as_current = Math.floor(x / (width / numslides)) + 1;
+
+        if( as_current != as_previous ){
+          as_previous = as_current;
+          changeImage();
+        }
+        return false;
+    });
+}
