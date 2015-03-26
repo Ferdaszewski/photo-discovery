8e8f370557c63fe3849a53b3f01c194c86ae9cb0
Joshua Ferdaszewski <ferdaszewski@gmail.com>
2015-03-26 16:42:13 -0700

    Photo del confirmation

--- static/js/update_album.js
+++ static/js/update_album.js
@@ -1,13 +1,31 @@
 $( '#photo-wrapper' ).click(function( event ) {
     if ( $(event.target).hasClass('delete-btn') ) {
         event.preventDefault();
-
-        // send AJAX call to delete the album
         var element_id = $(event.target).data('photo-id')
         var csrf_token = getCookie('csrftoken');
-        $.post('', {id: element_id, csrfmiddlewaretoken: csrf_token}, function( resp ) {
-            // Remove deleted photo from the DOM
-            $("li." + element_id).fadeOut(duration=1000, complete=function() {this.remove()});
+
+        // Open modal conformation window
+        $( '#del-confirm-modal' ).foundation( 'reveal', 'open' );
+
+        // Set listener for modal close button
+        $( '#del-confirm-modal .close-modal' ).click(function( event ) {
+            event.preventDefault();
+            $( '#del-confirm-modal' ).foundation( 'reveal', 'close' );
+        });
+
+        //Set listener for delete conformation button on modal window
+        $( '#del-confirm-modal .delete-confirm-btn' ).click(function( event ) {
+            event.preventDefault();
+
+            // send AJAX call to delete the album
+            $.post('', {id: element_id, csrfmiddlewaretoken: csrf_token}, function( resp ) {
+
+                // Remove deleted photo from the DOM
+                $("li." + element_id).fadeOut(duration=1000, complete=function() {this.remove()});
+
+                // Close modal window
+                $( '#del-confirm-modal' ).foundation( 'reveal', 'close' );                
+            });
         });
     } else {
         return;
--- static/js/update_albums.js
+++ static/js/update_albums.js
@@ -16,7 +16,6 @@ $( '#album-wrapper' ).click(function( event ) {
         // Set listener for delete conformation button on modal window
         $( '#del-confirm-modal .delete-confirm-btn' ).click(function( event ) {
             event.preventDefault();
-            console.log("in del confirm");
 
             // send AJAX call to delete the album
             $.post('', {id: element_id, csrfmiddlewaretoken: csrf_token}, function( resp ) {
